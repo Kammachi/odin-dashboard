639e64f574f3b45fbd946cd18e1748256c2f4fb4
Kammachi <126817246+Kammachi@users.noreply.github.com>
2024-05-22 20:58:06 +0200

    Add theme change option

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-* {
+const root = document.querySelector(':root');
+root.setAttribute("class", "light");
+
+const svgs = document.querySelectorAll('.theme-icon');
+
+
+function changeTheme() {
+    root.setAttribute("class", root.className == "dark" ? "light" : "dark");
     
+    svgs.forEach((svg) => {
+        svg.setAttribute("fill", svg.getAttribute("fill") == "black" ? "white" : "black");
+    });
 }
 
+
+document.querySelector('.theme-change').addEventListener('click', changeTheme);
